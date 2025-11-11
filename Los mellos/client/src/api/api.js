b0ce673cd5c017ad6f9521110897da73c6a30767
daniel-cpu818 <115967620+daniel-cpu818@users.noreply.github.com>
2025-11-11 00:07:30 -0500

    Delete Los mellos directory

--- Los mellos/client/src/api/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:4000/api",
-  withCredentials: true, // 👈 importante si usas tokens o cookies
-});
-
-export default api;
